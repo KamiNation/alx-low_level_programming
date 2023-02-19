59099c3dc287965a9647be952cca025f6eb62a5e
KamiNation <israeladedamola47@gmail.com>
2023-02-19 15:39:36 -0800

    fixed 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,15 @@
 int main(void)
 {
 	char c;
-	short s;
 	int i;
-	long l;
+	long li;
+	long long lli;
 	float f;
 
 	printf("The size of char is %ld byte(s)\n", sizeof(c));
-	printf("The size of short is %ld byte(s)\n", sizeof(s));
 	printf("The szie of int is %ld byte(s)\n", sizeof(i));
-	printf("The size of long is %ld byte(s)\n", sizeof(l));
+	printf("The size of long is %ld byte(s)\n", sizeof(li));
+	printf("The size of long long is %ld byte(s)\n" sizeof(lli);
 	printf("The sixe of float is %ld byte(s)\n", sizeof(f));
 	return (0);
 }

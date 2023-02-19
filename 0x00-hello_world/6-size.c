3b28adcd700d494d2c5ddf1f94143e193b3f3ba9
KamiNation <israeladedamola47@gmail.com>
2023-02-19 15:26:24 -0800

    fixed: 4,5,6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+* main - print the size of the following type variables: char, short, int, long, float
+*
+* Description: using the main function
+* this program prints the size of type var
+* Return: 0
+*/
 int main(void)
 {
 	char c;
@@ -11,6 +19,6 @@ int main(void)
 	printf("The size of short is %ld byte\n", sizeof(s));
 	printf("The szie of int is %ld byte\n", sizeof(i));
 	printf("The size of long is %ld byte\n", sizeof(l));
-        printf("The sixe of float is %ld byte\n", sizeof(f));
-        return (0);
-}	
+	printf("The sixe of float is %ld byte\n", sizeof(f));
+	return (0);
+}

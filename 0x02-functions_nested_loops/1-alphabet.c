726737769adb1221e53e78e2ec9d4f8d56699b5f
Israel Adedamola Adebayo <83586972+KamiNation@users.noreply.github.com>
2023-02-21 05:42:27 -0500

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ * in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	_putchar(alphabet);
+	}
+	_putchar('\n');
+}

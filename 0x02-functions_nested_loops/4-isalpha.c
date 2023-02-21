c9f713de730fc32096b8d50c732f2c3e815d2195
Israel Adedamola Adebayo <83586972+KamiNation@users.noreply.github.com>
2023-02-21 10:19:20 -0500

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - checks if the strung is 
+* an alphabet
+*/
+
+int _isalpha(int c)
+{
+  if ((c>=97 && c<=122) || (c>=65 && c<=90))
+  {
+    return (0);
+  }
+  return (0);
+}

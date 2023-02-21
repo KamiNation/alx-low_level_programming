0099cde3ef2d06fbc7121feed5087813d5f036e0
Israel Adedamola Adebayo <83586972+KamiNation@users.noreply.github.com>
2023-02-21 09:58:42 -0500

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - checks whether a char is in 
+* upper or lower case.
+*/
+
+int _islower(int c)
+{
+  if(ch>=97 && ch<=122)
+  {
+    return (1);
+    }
+  return (0);
+}

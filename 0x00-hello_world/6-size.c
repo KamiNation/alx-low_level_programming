4568d0fe0a98af2b77296c9e9908e07867737d88
KamiNation <israeladedamola47@gmail.com>
2023-02-21 00:39:24 -0800

    added: 0x01-variables_if_else_while

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("The size of char is %ld byte(s)\n", sizeof(c));
-	printf("The szie of int is %ld byte(s)\n", sizeof(i));
-	printf("The size of long is %ld byte(s)\n", sizeof(li));
-	printf("The size of long long is %ld byte(s)\n", sizeof(lli));
-	printf("The sixe of float is %ld byte(s)\n", sizeof(f));
+	printf("The size of char: %ld byte(s)\n", sizeof(c));
+	printf("The szie of int: %ld byte(s)\n", sizeof(i));
+	printf("The size of long: %ld byte(s)\n", sizeof(li));
+	printf("The size of long long: %ld byte(s)\n", sizeof(lli));
+	printf("The sixe of float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }

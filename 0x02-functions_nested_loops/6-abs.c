0d2035a19afac74fa1c8e4df644860f127e9d846
Israel Adedamola Adebayo <83586972+KamiNation@users.noreply.github.com>
2023-02-22 02:35:50 -0500

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - program to compute 
+* the abs value of an int
+*/
+
+int _abs(int n)
+{
+  if (n < 0)
+  {
+    n = -n;
+    return (n);
+  } 
+}
